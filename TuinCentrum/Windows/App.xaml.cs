1faae68d54d0fa7b5f494d3ab5c9df4f77f5aa43
JosimCoussens <coussensjosim@yahoo.com>
2024-05-12 15:35:57 +0200

    New stable build

---
+++ TuinCentrum/Windows/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TuinCentrum.Windows {
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        #region Properties
+
+        private string _connectionString { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public App() {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((context, configurationBuilder) => {
+                    configurationBuilder.SetBasePath(context.HostingEnvironment.ContentRootPath);
+                    configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    configurationBuilder.AddEnvironmentVariables();
+                })
+                .Build();
+
+            // Retrieve the connection string from the configuration
+            _connectionString = host.Services.GetRequiredService<IConfiguration>().GetConnectionString("TuinCentrumDbConnectionString");
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            e.Handled = true;
+        }
+
+        private async void OnStartup(object sender, StartupEventArgs e) {
+            // Maak en toon dashboard window
+            var dashboard = new dashboard();
+            dashboard.Show();
+        }
+
+        private async void OnExit(object sender, ExitEventArgs e) {
+        }
+
+        #endregion Methods
+    }
+}
--- TuinCentrum/configuration.cs
+++ TuinCentrum/configuration.cs
@@ -1,45 +1,47 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices.JavaScript;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using Windows.UI.Core;
+﻿//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.InteropServices.JavaScript;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using TuinCentrum.Windows;
+//using Windows.UI.Core;
+//using ZendeskApi_v2.Requests.HelpCenter;
 
-namespace TuinCentrum {
+//namespace TuinCentrum {
+//    internal class Configuration {
+//        public string _connectionString { get; private set; }
 
-    internal class Configuration {
-        public string _connectionString { get; private set; }
+//        public Configuration() {
+//            var host = new HostBuilder()
+//                .ConfigureAppConfiguration((context, configurationBuilder) => {
+//                    configurationBuilder.SetBasePath(context.HostingEnvironment.ContentRootPath);
+//                    configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+//                    configurationBuilder.AddEnvironmentVariables();
+//                })
+//                .Build();
 
-        public Configuration() {
-            var host = new HostBuilder()
-                .ConfigureAppConfiguration((context, configurationBuilder) => {
-                    configurationBuilder.SetBasePath(context.HostingEnvironment.ContentRootPath);
-                    configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                    configurationBuilder.AddEnvironmentVariables();
-                })
-                .Build();
+//            // Retrieve the connection string from the configuration
+//            _connectionString = host.Services.GetRequiredService<IConfiguration>().GetConnectionString("TuinCentrumDbConnectionString");
+//        }
+//    }
 
-            // Retrieve the connection string from the configuration
-            _connectionString = host.Services.GetRequiredService<IConfiguration>().GetConnectionString("TuinCentrumDbConnectionString");
-        }
-    }
-
-    internal class Program {
-
-        private static void Main(string[] args) {
-            var config = new Configuration();
-            string connectionString = config._connectionString;
-
-            // Now you can use the connection string as needed
-            MessageBox.Show(connectionString);
-        }
-    }
-}
+//    internal class Program {
+//        private static void Main(string[] args) {
+//            var config = new Configuration();
+//            string connectionString = config._connectionString;
+//            // Maak en toon dashboard window
+//            Application.Current.Dispatcher.Invoke(() => {
+//                var dashboard = new dashboard();
+//                dashboard.Show();
+//            });
+//        }
+//    }
+//}
